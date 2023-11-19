7db893352f4ee2243202a1f7cb06038b47b37827
Abdoulllahi <dabdoulahi@gmail.com>
2023-11-19 04:59:08 -0600

    technologies init

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+/**
+ * @ Author: Abdou Lahi DIOP - Copyright © 2023 Abdallah
+ * @ Creation Date: November 17, 2023 at 8:29:57 AM  CST
+ * @ Last Modification Date: November 19, 2023 at 4:27:08 AM  CST
+ * @ Modified by: Abdou Lahi DIOP
+ * @ Description:
+ */
+
+import { BallCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { technologies } from "../constants";
 
 const Tech = () => {
-  return (
-    <div>Tech</div>
-  )
+    return (
+        <div className="flex flex-row flex-wrap justify-center gap-10">
+            {technologies.map((technology) => (
+                <div className="w-28 h-28" key={technology.name}>
+                    <BallCanvas icon={technology.icon} />
+                </div>
+            ))}
+        </div>
+    )
 }
 
-export default Tech
+export default SectionWrapper(Tech, "");
--- src/constants/index.js
+++ src/constants/index.js
@@ -84,10 +84,10 @@ const technologies = [
         name: "React JS",
         icon: reactjs,
     },
-    {
-        name: "Redux Toolkit",
-        icon: redux,
-    },
+    // {
+    //     name: "Redux Toolkit",
+    //     icon: redux,
+    // },
     {
         name: "Tailwind CSS",
         icon: tailwind,
@@ -100,21 +100,21 @@ const technologies = [
         name: "MongoDB",
         icon: mongodb,
     },
-    {
-        name: "Three JS",
-        icon: threejs,
-    },
+    // {
+    //     name: "Three JS",
+    //     icon: threejs,
+    // },
     {
         name: "git",
         icon: git,
     },
     {
-        name: "figma",
-        icon: figma,
-    },
-    {
         name: "docker",
         icon: docker,
+    },
+    {
+        name: "figma",
+        icon: figma,
     },
 ];
 
