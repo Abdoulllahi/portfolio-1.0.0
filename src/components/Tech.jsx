34a0885d559b15cc793ba13b51b8974cdfd368c1
Abdoulllahi <dabdoulahi@gmail.com>
2023-11-19 05:37:25 -0600

    tech section done

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,10 +1,11 @@
 /**
  * @ Author: Abdou Lahi DIOP - Copyright © 2023 Abdallah
  * @ Creation Date: November 17, 2023 at 8:29:57 AM  CST
- * @ Last Modification Date: November 19, 2023 at 4:27:08 AM  CST
+ * @ Last Modification Date: November 19, 2023 at 5:37:17 AM  CST
  * @ Modified by: Abdou Lahi DIOP
  * @ Description:
  */
+/* eslint-disable react-refresh/only-export-components */
 
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
