d91a5b8be33569d71aaa73e072c81029434246bd
Abdoulllahi <dabdoulahi@gmail.com>
2023-12-12 00:20:56 -0600

    react slick configuration for deployment

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     plugins: [react()],
     build: {
         rollupOptions: {
-            external: ['react-slick'],
+            external: ['react-slick', 'slick-carousel/slick/slick.css'],
         },
     },
 })
