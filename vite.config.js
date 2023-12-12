b531096b8ae9000a56e3c6a4d991363f28c73004
Abdoulllahi <dabdoulahi@gmail.com>
2023-12-12 00:55:15 -0600

    Bug fix: changing node version

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     plugins: [react()],
     build: {
         rollupOptions: {
-            external: ['react-slick', 'slick-carousel/slick/slick.css'],
+            external: ['react-slick', 'slick-carousel'],
         },
     },
 })
